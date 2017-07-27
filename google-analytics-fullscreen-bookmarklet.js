5c61900a37fc0ff3732811aa486a878ef5259665
flacle <francislacle@gmail.com>
2017-07-27 13:25:18 -0400

    Added javascript bookmarklet.

---
+++ google-analytics-fullscreen-bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:var div=document.getElementById("ID-headerBar");if(div)div=div.style;if(div)void(div.display=div.display=="none"?"block":"none");var div=document.getElementById("ID-gafe4Nav");if(div)div=div.style;if(div)void(div.display=div.display=="none"?"block":"none");
